398e8ee3c0dbc52a594b4c93eba1be8b07b04dae
WhiteDLN <white14dl@gmail.com>
2026-04-07 00:38:16 -0300

    Ejemplos de punteros

--- tp1_1.c
+++ tp1_1.c
@@ -2,5 +2,21 @@
 
 int main(){
     printf("Hola Mundo");
+    int variable = 44;
+    int *puntero = &variable;
+
+     printf("Contenido del puntero (*puntero): %d\n", *puntero);
+    
+    
+    printf("Direccion almacenada en el puntero (puntero): %p\n", puntero);
+    
+    
+    printf("Direccion de la variable (&variable): %p\n", &variable);
+    
+    
+    printf("Direccion del puntero (&puntero): %p\n", &puntero);
+
+    printf("5) Tamaño de la variable (sizeof(variable)): %d bytes\n", sizeof(variable));
+
     return 0;
 }
